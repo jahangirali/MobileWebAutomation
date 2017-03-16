9e6b715a80602a10a8d7a13cd742a5f8366bc10c
jahangira <jahangir.ali@easyjet.com>
2017-03-16 13:49:38 +0000

    initial

---
+++ ConsoleApplication1/ConsoleApplication1/Tests/TestBase.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace MobileWebAutomation.Tests
+{
+    class TestBase
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver Driver = new ChromeDriver();
+            Driver.Navigate().GoToUrl("https://m.easyjet.com/m2p-ej/s/");
+        }
+    }
+}
